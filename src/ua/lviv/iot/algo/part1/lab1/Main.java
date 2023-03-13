3221486464283bbcc8bb95c14d54a2de894a0de4
Bogdan <119266071+bogdan9876@users.noreply.github.com>
2023-03-13 15:10:34 +0200

    code after review

--- src/ua/lviv/iot/algo/part1/lab1/Main.java
+++ src/ua/lviv/iot/algo/part1/lab1/Main.java
@@ -7,7 +7,6 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-
 public class Printer {
     private String model;
     private String type;
@@ -18,21 +17,14 @@ public class Printer {
 
     public void print(int pages) {
         if (paperCount >= pages) {
-            paperCount -= pages;
-            System.out.println("Printing " + pages + " pages.");
-        } else {
-            System.out.println("Not enough paper in tray.");
+            paperCount =paperCount-pages;
         }
     }
 
     public void loadPaper(int count) {
         int availableSpace = paperTrayCapacity - paperCount;
         if (count <= availableSpace) {
-            paperCount += count;
-        }
-        else {
-            paperCount = paperTrayCapacity;
-            System.out.println("Paper tray is full.");
+            paperCount = count+paperCount;
         }
     }
 
@@ -46,15 +38,10 @@ public class Printer {
     }
 
     public static void main(String[] args) {
-        Printer[] printers = new Printer[4];
-        printers[0] = new Printer();
-        printers[1] = new Printer("Epson", "Inkjet", true, false, 50, 20);
-        printers[2] = getInstance();
-        printers[3] = getInstance();
-
-        for (Printer printer : printers) {
+        Printer[] printers = {new Printer(),new Printer("Epson", "Inkjet", true, false, 50, 20)
+                ,getInstance(),getInstance()};
+        for(Printer printer : printers) {
             System.out.println(printer.toString());
         }
     }
-
 }
